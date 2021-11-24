7b09a4cf85479ebde392cb8af90a11a96de23668
Astrti Lariza Garcia Castillo <90290485+LarizaG@users.noreply.github.com>
2021-11-24 16:50:02 -0600

    Add files via upload

---
+++ funcion_trascendente10.m
@@ -0,0 +1,22 @@
+% Script de Octave
+% Titulo      :Funciones trascendentes
+% Author      :Astrit Lariza Garcia Castillo
+% Date        :20211123
+% Version     :1
+% Usage       :octave> cd /path/
+% Notes       :Requiere aplicacion octave 
+
+
+clear
+
+
+x= -10:1:10;
+ 
+hx= csc(x);
+
+plot(x, hx);
+
+title(['Funcion 10= de ningun tipo']);
+disp("Esta funcion no es subrayectiva por que hay un elemento del dominio que no tiene valor  respecto al codominio");
+disp("Esta funcion no es inyectiva porque al trazar una rectar hacia el eje x, esta corta mas de un punto la recta");
+disp("Por lo tanto no puede ser biyectiva por que no cumple con ambas clasificaciones");
