81f2d7fec739aecc99210481613059b3b8e30645
Astrti Lariza Garcia Castillo <90290485+LarizaG@users.noreply.github.com>
2021-11-24 16:46:28 -0600

    Add files via upload

---
+++ funcion_trascendente5.m
@@ -0,0 +1,21 @@
+% Script de Octave
+% Titulo      :Funciones trascendentes
+% Author      :Astrit Lariza Garcia Castillo
+% Date        :20211123
+% Version     :1
+% Usage       :octave> cd /path/
+% Notes       :Requiere aplicacion octave 
+
+
+clear
+
+x= 5:1:10
+
+fx= log(x)/log(3);
+
+plot(x, fx);
+
+title(["Funcion 5= biyectiva"]);
+disp("Esta funcion es subrayectiva por que ningun elemento del codominio y sobra respecto al dominio");
+disp("Esta funcion es inyectiva porque al trazar una rectar hacia el eje x, esta no  corta mas de un punto la recta");
+disp("Por lo tanto es  biyectiva por que cumple con ambas clasificaciones");
